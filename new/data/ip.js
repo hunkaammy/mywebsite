821907d40543d37f7eec4c9fff17b139039c9a6f
hunksammy <hindustantoysrs@gmail.com>
2021-09-09 11:56:26 +0530

    t

---
+++ new/data/ip.js
@@ -0,0 +1,37 @@
+new Vue({
+	el: "#app",
+	data() {
+		return {
+			response: null,
+			ip: null,
+			useragent: null,
+			ts: null
+		};
+	},
+	watch: {
+		response: function () {
+			this.ip = this.response.substring(
+				this.response.search("ip=") + 3,
+				this.response.search("ts=")
+			);
+			this.ts = this.response.substring(
+				this.response.search("ts=") + 3,
+				this.response.search("visit_scheme=")
+			);
+			this.useragent = this.response.substring(
+				this.response.search("uag=") + 4,
+				this.response.search("colo=")
+			);
+		}
+	},
+	computed: {
+		tsFormatted() {
+			return new Date(this.ts * 1000);
+		}
+	},
+	mounted() {
+		axios
+			.get("https://www.cloudflare.com/cdn-cgi/trace")
+			.then((response) => (this.response = response.data));
+	}
+});
