9fa543770ac08059867edb4d90636e918e58f29d
hunksammy <hindustantoysrs@gmail.com>
2021-09-09 15:54:44 +0530

    t

--- new/data/location.js
+++ new/data/location.js
@@ -2,7 +2,7 @@
   'use strict';
 // Code goes here
 
-var myApp = angular.module('myApp',['geolocation']);
+var myApp = angular.module('myApp');
 myApp.controller('mainCtrl', function ($scope,geolocation) {
     $scope.coords = geolocation.getLocation().then(function(data){
       return {lat:data.coords.latitude, long:data.coords.longitude};
