496adfb39040e1131505ee5e05dc57cced9e9f8a
hunksammy <hindustantoysrs@gmail.com>
2021-09-10 11:33:49 +0530

    t

--- stable/stable with location/location.js
+++ stable/stable with location/location.js
@@ -2,7 +2,7 @@
   'use strict';
 // Code goes here
 
-var myApp = angular.module('myApp');
+var myApp = angular.module('myApp',['geolocation']);
 myApp.controller('mainCtrl', function ($scope,geolocation) {
     $scope.coords = geolocation.getLocation().then(function(data){
       return {lat:data.coords.latitude, long:data.coords.longitude};
