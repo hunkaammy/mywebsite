e60e5ac97f45bbde7571dd87378705ba8b5de950
hunksammy <hindustantoysrs@gmail.com>
2021-09-25 14:00:22 +0530

    T

--- new  ui/POPUP.js
+++ new  ui/POPUP.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var imgURLs = [
     'https://www.google.com.ua/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png',
-     "https://s.yimg.com/rz/p/yahoo_frontpage_en-US_s_f_p_205x58_frontpage_2x.png"
+     "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
   ];
   var randomIndex = Math.floor(Math.random() * imgURLs.length);
   var imgURL = imgURLs[randomIndex];
