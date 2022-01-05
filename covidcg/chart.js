7e8e599804c47458724256d954ce140702293f96
hunksammy <hindustantoysrs@gmail.com>
2022-01-05 19:45:00 +0530

    t

--- covidcg/chart.js
+++ covidcg/chart.js
@@ -6,8 +6,7 @@ let revChart = new Chart(ctx, {
   type: "line",
   data: {
     labels: [
-	"dec 25",
-      "dec 26",
+	 "dec 26",
       "dec 27",
       "dec 28",
       "dec 29",
@@ -15,7 +14,8 @@ let revChart = new Chart(ctx, {
       "dec 31",
       "jan 01",
       "jan 02",
-"jan 03"
+"jan 03",
+"jan 04"
      
     ],
     datasets: [
@@ -23,19 +23,19 @@ let revChart = new Chart(ctx, {
         label: "positive",
         borderColor: "red",
         backgroundColor: "rgba(235, 247, 245, 0.5)",
-        data: [37, 46, 39 , 69, 106, 150, 190, 279, 290, 698]
+        data: [46, 39 , 69, 106, 150, 190, 279, 290, 698, 1059]
       },
       {
         label: "recovered",
         borderColor: "green",
         backgroundColor: "rgba(233, 238, 253, 0.5)",
-        data: [ 20, 29, 33, 19, 36, 16, 18, 30, 34, 29]
+        data: [ 29, 33, 19, 36, 16, 18, 30, 34, 29, 21]
       },
 	   {
         label: "death",
         borderColor: "black",
         backgroundColor: "rgba(233, 238, 253, 0.5)",
-        data: [ 0, 0, 1, 2, 0, 0, 0, 1, 0, 0]
+        data: [ 0, 1, 2, 0, 0, 0, 1, 0, 0, 3]
       } 
     ]
   },
