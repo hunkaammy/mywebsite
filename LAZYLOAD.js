438db9440a09b18fadb6fc115627568bc43495a6
hunksammy <hindustantoysrs@gmail.com>
2022-03-17 12:32:52 +0530

    t

--- LAZYLOAD.js
+++ LAZYLOAD.js
@@ -1,45 +1,17 @@
+window.addEventListener("scroll", function() { onScrollDiv() });
+	window.addEventListener("DOMContentLoaded", function() { onScrollDiv() });
 
-function logElementEvent(eventName, element) {
-console.log(Date.now(), eventName, element.getAttribute("data-src"));
-}
-var callback_enter = function(element) {
-logElementEvent("🔑 ENTERED", element);
-};
-var callback_exit = function(element) {
-logElementEvent("🚪 EXITED", element);
-};
-var callback_loading = function(element) {
-logElementEvent("⌚ LOADING", element);
-};
-var callback_loaded = function(element) {
-logElementEvent("👍 LOADED", element);
-};
-var callback_error = function(element) {
-logElementEvent("💀 ERROR", element);
-element.src =
-"https://via.placeholder.com/440x560/?text=Error+Placeholder";
-};
-var callback_finish = function() {
-logElementEvent("✔️ FINISHED", document.documentElement);
-};
-var callback_cancel = function(element) {
-logElementEvent("🔥 CANCEL", element);
-};
-window.lazyLoadOptions = {
-threshold: 0,
-// Assign the callbacks defined above
-callback_enter: callback_enter,
-callback_exit: callback_exit,
-callback_cancel: callback_cancel,
-callback_loading: callback_loading,
-callback_loaded: callback_loaded,
-callback_error: callback_error,
-callback_finish: callback_finish
-};
-window.addEventListener(
-"LazyLoad::Initialized",
-function(e) {
-console.log(e.detail.instance);
-},
-false
-);
+	function onScrollDiv() {
+		var images = document.querySelectorAll('.lazyload');
+		for (var i=0, nb=images.length ; i <nb ; i++) {
+			var img = images[i]
+			var rect = img.getBoundingClientRect();
+			var isVisible = ((rect.top - window.innerHeight) < 500 && (rect.bottom) > -50 ) ? true : false ;
+
+			if (isVisible) {
+				if (!img.src) {
+					img.src = img.dataset.src;
+				}
+			}
+		}
+	}
