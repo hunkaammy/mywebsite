9a5073ec1cbcc6cf89364b7154e2ba7f0fed1cbf
hunksammy <hindustantoysrs@gmail.com>
2022-07-31 14:09:34 +0530

    t

---
+++ price/script.js
@@ -0,0 +1,16 @@
+var numOne = document.getElementById("num-one");
+var numTwo = document.getElementById("num-two");
+var numThree = document.getElementById("num-three");
+var addSum = document. getElementById("add-sum");
+
+numOne.addEventListener("input", add);
+numTwo.addEventListener("input", add);
+numThree.addEventListener("input", add);
+
+function add(){
+  var one = Number(numOne.value) || 0;
+  var two = Number(numTwo.value) || 0;
+  var three = Number(numThree.value) || 0;
+  var sum = Number(one*two+three);
+  addSum.innerHTML = "Your sum is: " + sum;
+}
