a6888ded9a07a1a3d5e6848e9438434a3fd37861
hunksammy <hindustantoysrs@gmail.com>
2022-08-22 21:20:39 +0530

    t

--- price/script.js
+++ price/script.js
@@ -1,1 +1,27 @@
-jplist.init();
+jQuery(document).ready(function () {
+  let minPrice = jQuery("#minPrice");
+  let maxPrice = jQuery("#maxPrice");
+
+  function filterPrices() {
+    jQuery(".container .item").each(function () {
+      let itemPrice = jQuery(this).children("#price").html();
+      itemPrice = +itemPrice.slice(1);
+
+      if (minPrice.val() <= +itemPrice && maxPrice.val() >= +itemPrice) {
+        jQuery(this).css("display", "block");
+      } else if (!minPrice.val() || !maxPrice.val()) {
+        jQuery(this).css("display", "block");
+      } else {
+        jQuery(this).css("display", "none");
+      }
+    });
+  }
+
+  jQuery(minPrice).keyup(function () {
+    filterPrices();
+  });
+
+  jQuery(maxPrice).keyup(function () {
+    filterPrices();
+  });
+});
